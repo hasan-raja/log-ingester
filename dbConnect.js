c145fd94850c92d898c6af4f6be409fedace6885
hasan-raja <hasanraja2199@gmail.com>
2023-11-20 20:57:24 +0530

    changing mongo connection 2

--- dbConnect.js
+++ dbConnect.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const dbConnect = () => {
-    // const connectionParams = { useNewUrlParser: true };
-	const connectionParams = {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		ssl: true,
-		sslValidate: false, // Disable SSL validation for testing purposes
-		// Other connection options...
-	  };
+    const connectionParams = { useNewUrlParser: true };
+	// const connectionParams = {
+	// 	useNewUrlParser: true,
+	// 	useUnifiedTopology: true,
+	// 	ssl: true,
+	// 	sslValidate: false, // Disable SSL validation for testing purposes
+	// 	// Other connection options...
+	//   };
     mongoose.connect(process.env.DB, connectionParams);
 
     mongoose.connection.on("connected", () => {
